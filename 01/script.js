52f54c43e5d7096b996bd718f908bbf7e6afd35c
Shreekantha3 <biradarshreekantha@gmail.com>
2023-11-12 07:01:23 -0600

    Case and conditional switch

---
+++ 01/script.js
@@ -0,0 +1,11 @@
+const age = 15
+
+if(age >= 80) {
+    console.log('you are a senior citizen, you can vote')   
+}
+else if(age >= 80) {
+    console.log('Yes you can vote')
+}
+else{
+    console.log('No you cant vote')
+}
