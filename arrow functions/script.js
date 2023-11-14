0e683b142bade758616ee2335561b0ddf21962c6
Shreekantha3 <biradarshreekantha@gmail.com>
2023-11-14 02:20:11 -0600

    Arrow functions

---
+++ arrow functions/script.js
@@ -0,0 +1,38 @@
+// Arrow function in javascript
+
+
+// example 1
+
+const add = (a, b) => a + b
+
+res = add(5,10)
+console.log(add(3,4))
+console.log(res)
+
+// print Hello
+
+const sayHello = () => {console.log('Hello How are you shree')}
+
+sayHello()
+sayHello()
+sayHello()
+sayHello()
+sayHello()
+
+
+// multi line arrow function
+
+
+const multiply = (a, b) =>{
+    const result = a * b;
+    return result
+}
+
+const result = multiply(33, 4);
+console.log(result); // Output: 12
+
+
+
+
+
+
